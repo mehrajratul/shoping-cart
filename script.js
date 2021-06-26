58e7ec41c9997c2a92ccfcfadbf9f22a6f76e482
mehrajratul <mehrajratul@gmail.com>
2021-06-26 18:25:22 +0600

    subtotal feature added

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ btnAdd.addEventListener('click', () => {
     input.value = parseInt(input.value) + 1;
     let addPhonePrice = document.getElementById('addPhonePrice');
     let price = 1219;
-    addPhonePrice.innerText = price*input.value;
+    addPhonePrice.innerText = price * input.value;
+    let subTotal = document.querySelector('#subTotal');
+    subTotal.innerText = parseFloat(addPhonePrice.innerText) + parseFloat(addCasePrice.innerText);
 });
 btnSubtract.addEventListener('click', () => {
     input.value = parseInt(input.value) - 1;
     let addPhonePrice = document.getElementById('addPhonePrice');
     let NegPrice = -(-1219);
-    addPhonePrice.innerText = NegPrice*input.value;
+    addPhonePrice.innerText = NegPrice * input.value;
+    subTotal.innerText = parseFloat(addPhonePrice.innerText) + parseFloat(addCasePrice.innerText);
 });
-// let addPhonePrice = document.getElementById('addPhonePrice');
-// addPhonePrice.innerText = ;
 
 
 let btnAddCase = document.querySelector('#addCase');
@@ -24,7 +25,14 @@ let caseInput = document.querySelector('#caseInput');
 
 btnAddCase.addEventListener('click', () => {
     caseInput.value = parseInt(caseInput.value) + 1;
-})
+    const addCasePrice = document.getElementById('addCasePrice');
+    const casePrice = 59;
+    addCasePrice.innerText = casePrice * caseInput.value;
+    subTotal.innerText = parseFloat(addCasePrice.innerText) + parseFloat(addPhonePrice.innerText);
+});
 btnSubtractCase.addEventListener('click', () => {
     caseInput.value = parseInt(caseInput.value) - 1;
+    const negCasePrice = -(-59);
+    addCasePrice.innerText = negCasePrice * caseInput.value;
+    subTotal.innerText = parseFloat(addCasePrice.innerText) + parseFloat(addPhonePrice.innerText);
 });
