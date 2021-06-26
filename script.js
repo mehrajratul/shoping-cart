d1ea5ed560a5783876afdfc463504af4e83ff54b
mehrajratul <mehrajratul@gmail.com>
2021-06-26 17:15:35 +0600

    plus and minus buttons are working

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello cart');
+let btnAdd = document.querySelector('#add');
+let btnSubtract = document.querySelector('#subtract');
+let input = document.querySelector('#input');
+
+btnAdd.addEventListener('click', ()=>{
+    input.value = parseInt(input.value) + 1;
+});
+btnSubtract.addEventListener('click', ()=>{
+    input.value = parseInt(input.value) - 1;
+});
+
+
+let btnAddCase = document.querySelector('#addCase');
+let btnSubtractCase = document.querySelector('#subtractCase');
+let caseInput = document.querySelector('#caseInput');
+
+btnAddCase.addEventListener('click', ()=>{
+    caseInput.value = parseInt(caseInput.value) + 1;
+})
+btnSubtractCase.addEventListener('click', ()=>{
+    caseInput.value = parseInt(caseInput.value) - 1;
+});
