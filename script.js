be42b42292004b6a235c5778490d4b6e0e24a78e
mehrajratul <mehrajratul@gmail.com>
2021-06-26 17:46:22 +0600

    first part is almost completed

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ let btnAdd = document.querySelector('#add');
 let btnSubtract = document.querySelector('#subtract');
 let input = document.querySelector('#input');
 
-btnAdd.addEventListener('click', ()=>{
+btnAdd.addEventListener('click', () => {
     input.value = parseInt(input.value) + 1;
+    let addPhonePrice = document.getElementById('addPhonePrice');
+    let price = 1219;
+    addPhonePrice.innerText = price*input.value;
 });
-btnSubtract.addEventListener('click', ()=>{
+btnSubtract.addEventListener('click', () => {
     input.value = parseInt(input.value) - 1;
+    let addPhonePrice = document.getElementById('addPhonePrice');
+    let NegPrice = -(-1219);
+    addPhonePrice.innerText = NegPrice*input.value;
 });
+// let addPhonePrice = document.getElementById('addPhonePrice');
+// addPhonePrice.innerText = ;
 
 
 let btnAddCase = document.querySelector('#addCase');
 let btnSubtractCase = document.querySelector('#subtractCase');
 let caseInput = document.querySelector('#caseInput');
 
-btnAddCase.addEventListener('click', ()=>{
+btnAddCase.addEventListener('click', () => {
     caseInput.value = parseInt(caseInput.value) + 1;
 })
-btnSubtractCase.addEventListener('click', ()=>{
+btnSubtractCase.addEventListener('click', () => {
     caseInput.value = parseInt(caseInput.value) - 1;
 });
